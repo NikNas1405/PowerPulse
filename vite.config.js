e3c4e2b002aee8309861cb04878771488ee971d3
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2024-01-11 21:37:29 +0200

    base config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-      plugins: [react(), svgr()],
-      resolve: {
-        alias: {
-          src: '/src',
-          components: '/src/components',
-          pages: '/src/pages',
-          assets: '/src/assets'
-        },
-      },
-      base: '/react_vite/',
-    });
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
+    },
+  },
+  base: '/PowerPulse/',
+});
