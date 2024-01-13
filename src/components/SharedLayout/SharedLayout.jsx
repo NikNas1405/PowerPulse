6b5f589874c452462ecd0d6b8e228e2b2da33eff
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2024-01-13 21:07:05 +0200

    SharedLayout updated
    react-toastify styles added

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Header } from '../Header/Header';
 import { Loader } from '../Loader/Loader';
