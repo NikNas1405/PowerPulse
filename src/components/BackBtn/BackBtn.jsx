9b10322b3d250b83f3fdb24a6773192a04f2b67a
TanyaMosk <tanya856227@gmail.com>
2024-01-24 22:37:08 +0100

    update_btn

--- src/components/BackBtn/BackBtn.jsx
+++ src/components/BackBtn/BackBtn.jsx
@@ -1,7 +1,40 @@
-import { BackBtn } from './BackBtn.Styled';
+// import { BackBtn } from './BackBtn.Styled';
 
-// import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 
-export const GoBackBtn = ({ to, children }) => {
-  return <BackBtn to={to}>{children}</BackBtn>;
+// export const GoBackBtn = ({ to, children }) => {
+//   return <BackBtn to={to}>{children}</BackBtn>;
+// };
+
+const GoBackBtn = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  // const { from } = location.state || {
+  //   from: { pathname: '/exercises/Body parts' },
+  // };
+  // console.log('from:', from.pathname);
+  // console.log('location =>', location);
+
+  const backLinkLocationRef = useRef(
+    location.state?.from || '/exercises/Body parts'
+  );
+
+  useEffect(() => {
+    if (location.pathname !== backLinkLocationRef.current) {
+      backLinkLocationRef.current = location.pathname;
+    }
+  }, [location]);
+
+  const handleClickBackBtn = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClickBackBtn}> Go back</button>
+    </div>
+  );
 };
+
+export default GoBackBtn;
